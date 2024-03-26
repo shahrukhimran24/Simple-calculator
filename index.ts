f9f80e5689ebd8e782641edddfdda5aa407bc2ac
Shahrukh <sharokkha24@gmail.com>
2024-03-26 15:09:58 +0000

    Adding a Simple CLI Calculator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+//printing a Wellcome Message
+console.log("\n\tWellcome To \'Shahrukh khan\'- CLI simple Calculator");
+
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+  },
+]);
+//CONDITIONAL STATEMENT
+if(answer.operator ==="ADDITION"){
+ console.log(answer.firstNumber+answer.secondNumber);
+}else if(answer.operator ==="SUBTRACTION"){
+  console.log(answer.firstNumber-answer.secondNumber);
+ }else if(answer.operator ==="MULTIPLICATION"){
+  console.log(answer.firstNumber*answer.secondNumber);
+ }else if(answer.operator ==="DIVISION"){
+  console.log(answer.firstNumber/answer.secondNumber);
+ } 
+ else{"print select correct operator"}
